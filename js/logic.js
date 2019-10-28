bd42263ded63eaa2748eec19c880586c66a6882e
Alriosa <carlos.alfredo.rios15@gmail.com>
2019-10-27 20:04:46 -0600

    6

--- js/logic.js
+++ js/logic.js
@@ -3,3 +3,9 @@ console.log('Logic is connected');
 document.getElementById("textBoxSecOne").value = "I insert this via JS";
 
 document.getElementById("textSecOne").innerHTML = 5 + 6;
+
+
+function windowPopUp() {
+    window.alert("This is a Window alert");
+    console.log("Funciona");
+}
