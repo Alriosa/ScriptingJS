5f12f4890ca89cc4c6c5d2575bc0801b3099e9c6
Alriosa <carlos.alfredo.rios15@gmail.com>
2019-10-28 21:23:38 -0600

    7

--- js/logic.js
+++ js/logic.js
@@ -1,11 +1,24 @@
 console.log('Logic is connected');
-
 document.getElementById("textBoxSecOne").value = "I insert this via JS";
-
 document.getElementById("textSecOne").innerHTML = 5 + 6;
+
+
+
+var date = new Date();
+
 
 
 function windowPopUp() {
     window.alert("This is a Window alert");
+
+}
+
+function getTime() {
+
+    var hour = date.getHours();
+    var minute = date.getMinutes();
+    var second = data.getSeconds();
+
+    document.getElementById("textBoxDateOne").value = hour + ":" + minute + ":" + second;
     console.log("Funciona");
 }
