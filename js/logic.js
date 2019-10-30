e154e504ab7f0251ede6101888647fd9bf142aa3
Alriosa <carlos.alfredo.rios15@gmail.com>
2019-10-30 14:54:29 -0600

    9

--- js/logic.js
+++ js/logic.js
@@ -2,26 +2,34 @@ console.log('Logic is connected');
 document.getElementById("textBoxSecOne").value = "I insert this via JS";
 document.getElementById("textSecOne").innerHTML = 5 + 6;
 
-
-
 var date = new Date();
-
-
 
 function windowPopUp() {
     window.alert("This is a Window alert");
-
 }
 
 function getTime() {
 
     var hour = date.getHours();
     var minute = date.getMinutes();
-    // var second = data.getSeconds(); (DEPRECATED)
 
     var hourUTC = date.getUTCHours();
-    var minuteUTC = data.getUTCMinutes();
+    //var minuteUTC = data.getUTCMinutes();
 
-    document.getElementById("textBoxDateOne").value = hour + ":" + minute + ":" + second;
+    var hourEST;
+    var minuteEST;
+
+    var hourPST;
+    var minutePST;
+
+    var hourCST;
+    var minuteCST;
+
+    var hourMST;
+    var minuteMST;
+
+
+    document.getElementById("textBoxDateOne").value = hour + ":" + minute;
+    document.getElementById("textBoxDateTwo").value = hourUTC + ":" + minute;
     console.log("Funciona");
 }
