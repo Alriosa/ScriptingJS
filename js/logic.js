73994544f36ffdf3b5b966183f47355a492e2ecb
Alriosa <carlos.alfredo.rios15@gmail.com>
2019-10-30 15:23:32 -0600

    11

--- js/logic.js
+++ js/logic.js
@@ -3,33 +3,51 @@ document.getElementById("textBoxSecOne").value = "I insert this via JS";
 document.getElementById("textSecOne").innerHTML = 5 + 6;
 
 var date = new Date();
+var hour = date.getHours();
+var minute = addZero(date.getMinutes());
 
-function windowPopUp() {
-    window.alert("This is a Window alert");
+var hourUTC = date.getUTCHours();
+//var minuteUTC = data.getUTCMinutes();
+
+var hourEST = date.getUTCHours();
+var minuteEST;
+
+var hourPST = date.getUTCHours();;
+var minutePST;
+
+var hourCST = date.getUTCHours();;
+var minuteCST;
+
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+function setEST() {
+
+}
+
+function setPST() {
+
+}
+
+function setCST() {
+
 }
 
 function getTime() {
 
-    var hour = date.getHours();
-    var minute = date.getMinutes();
-
-    var hourUTC = date.getUTCHours();
-    //var minuteUTC = data.getUTCMinutes();
-
-    var hourEST;
-    var minuteEST;
-
-    var hourPST;
-    var minutePST;
-
-    var hourCST;
-    var minuteCST;
-
-    var hourMST;
-    var minuteMST;
-
-
     document.getElementById("timeBoxMdt").value = hour + ":" + minute;
     document.getElementById("timeBoxUtc").value = hourUTC + ":" + minute;
+    document.getElementById("timeBoxEst").value = hourUTC + ":" + minute;
+    document.getElementById("timeBoxPst").value = hourUTC + ":" + minute;
+    document.getElementById("timeBoxCst").value = hourUTC + ":" + minute;
+    document.getElementById("timeBoxMdt").value = hour + ":" + minute;
     console.log("Funciona");
+}
+
+function windowPopUp() {
+    window.alert("This is a Window alert");
 }
