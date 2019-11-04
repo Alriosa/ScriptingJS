59344e7d826aa39f191bb9b4b981167b5b57be25
Alriosa <carlos.alfredo.rios15@gmail.com>
2019-11-04 13:43:16 -0600

    16

--- js/logic.js
+++ js/logic.js
@@ -3,9 +3,9 @@ document.getElementById("textBoxSecOne").value = "I inserted this via JS";
 document.getElementById("textSecOne").innerHTML = 5 + 6;
 
 
+//------------------------------//Inicio de logica de tiempo.//------------------------------//
 
 var date = new Date();
-
 var hour = date.getHours();
 var minute = addZero(date.getMinutes());
 var hourUTC = date.getHours() + 6;
@@ -14,14 +14,27 @@ var hourPST = date.getHours() - 1;
 var hourCST = date.getHours() + 1;
 
 
+//function validate24h(h){}(Aun no se si hacer esta funcion para validar)
+
+//Funcion que agrega un cero cuando el minuto es menor a 0. 
+
 function addZero(i) {
     if (i < 10) {
         i = "0" + i;
     }
     return i;
 }
+//esta funcion sirve para obtener el valor de las horas.
 
-function validate24h(h) {}
+function autoTimeRefresh(t) {
+    console.log("Hice refresh brother!");
+    document.getElementById("timeBoxMdt").value = hour + ":" + minute;
+    document.getElementById("timeBoxUtc").value = hourUTC + ":" + minute;
+    document.getElementById("timeBoxEst").value = hourEST + ":" + minute;
+    document.getElementById("timeBoxPst").value = hourPST + ":" + minute;
+    document.getElementById("timeBoxCst").value = hourCST + ":" + minute;
+    setTimeout("location.reload(true);", t);
+}
 
 function getTime() {
 
@@ -30,9 +43,31 @@ function getTime() {
     document.getElementById("timeBoxEst").value = hourEST + ":" + minute;
     document.getElementById("timeBoxPst").value = hourPST + ":" + minute;
     document.getElementById("timeBoxCst").value = hourCST + ":" + minute;
-    console.log("Funciona");
 
 }
+
+function timeTransform() {
+    document.getElementById("yourTimebox").value = hour + ":" + minute;
+}
+
+function customerTimeToMineUTC() {}
+
+function customerTimeToMineEST() {}
+
+function customerTimeToMinePST() {}
+
+function customerTimeToMineCST() {}
+//------------------------------//Fin de logica de tiempo.//------------------------------//
+
+
+
+
+function testFunction() {
+    console.log("La funcion si ha entrado");
+}
+
+
+
 
 function windowPopUp() {
     window.alert("This is a Window alert");
